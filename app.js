a8c8b9fceb6e4f97b04d91b1626da99c5b6bc3f0
FreyDv <ldfm0401@gmail.com>
2021-03-25 23:15:51 +0200

    fd

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ function x(node) {
   console.log(xa.tagName.toLowerCase() + ': ', xa.textContent)
   return xa
 }
-const title = x('title')
-const h1 = x('h1')
-const h2 = x('h2')
-const h3 = x('h3')
-const h4 = x('h4')
+// const title = x('title')
+// const h1 = x('h1')
+// const h2 = x('h2')
+// const h3 = x('h3')
+// const h4 = x('h4')
 ////квери селектор возврашаетт первый попавшийся подходящий елемент
 ////<h2 id="h2" class="h2-class">Лера не меша пожалуйста</h2>
 // const h21 = document.querySelector('h2') //поиск по тег найму
@@ -24,13 +24,58 @@ const h4 = x('h4')
 // h1.style.padding='2rem'
 
 var y = 1
-function node(n) {
-  n.textContent = 'Text cheng from js cod' + y++
+
+setTimeout(function() {node(h1,'Я начал изучать DOM')},1000)
+setTimeout(function() {node(h2,'И я нашел Видео Владилена Минина')},1500)
+setTimeout(function() {node(h3,'Видео очень полезные')},2000)
+setTimeout(function() {node(h4,'Скоро выучу!!')},2500)
+function node(n,text) {
+  if(text) n.textContent = text + y++
+  else n.textContent = y++
   n.style.color = 'red'
   n.style.textAlign = 'center'
   n.style.backgroundColor = 'black'
   n.style.padding = '2rem'
 }
+
+
+var ix=0;
+var ic
+var xc
+h1.onclick = () => {
+  setInterval(() => {
+    
+    
+    if(ix==15728638){
+      clearInterval()
+    }
+    ic = ix.toString(16)
+    
+    
+    switch(ic.length){
+      case 1:xc= '#00000'+ic 
+      break;
+      case 2:xc= '#0000'+ic
+      break;
+      case 3:xc= '#000'+ic
+      break;
+      case 4:xc= '#00'+ic
+      break;
+      case 5:xc= '#0'+ic
+      break;
+      case 6:{
+        xc= '#0'+ ic.substring(0,ic.length-1)
+      break;
+      }
+    }
+    console.log(xc)
+    
+     h1.style.backgroundColor=xc
+    ix+=1000
+  },15)
+}
+
+console.dir(ic)
 
 node(h1)
 node(h2)
